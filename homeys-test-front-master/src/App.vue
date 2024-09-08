2f7d7a3296f9d92b9ddaad2f2ea04ac6cfd12d24
skhali <sev.jocks@yahoo.fr>
2024-09-08 22:38:29 +0200

    ⚡ Etape 01 : ajout d'un composant BaseNotification bleu

---
+++ homeys-test-front-master/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import BaseNotification from './components/BaseNotification.vue';
+</script>
+
+<template>
+  <!-- Affichage d'une notification au choix : j'ai choisis la notification d'information -->
+  <main>
+    <BaseNotification
+    :title="'Modal Window'"
+    :message="'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sodales dictum nibh id commodo'"
+    ></BaseNotification>
+  </main>
+</template>
---
+++ homeys-test-front-master/src/components/BaseNotification.vue
@@ -0,0 +1,42 @@
+<template>
+    <!-- Template d'une notification :
+    La divison a été faite ainsi pour faciliter la disposition des différents
+    éléments en css.
+    -->
+    <div class="notification-content">
+        <!-- pas très flexible pour cette première étape mais ce le sera à l'étape 2 !-->
+        <img class="notification-icon" src="@/assets/icons/Info.svg" >
+        <div class="notification-header-body">
+            <div class="notification-header">
+                <h3 class="notification-title">{{ title }}</h3>
+                    <!-- j'avais pensé à utiliser la technique avec before et after pour créer une croix
+                     en css puis j'ai vu que ce caractère existait.-->
+                <button class="notification-close" > &#x2715;
+                </button>
+            </div>
+            <div class="notification-body">
+                {{message}}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    import { defineProps } from 'vue';
+
+    /* Props :
+        Pour l'instant nous n'en auront que deux :
+        - title : le titre de ma notification, un string requis
+        - message : le contenue de ma notification, un string requis
+    */
+    const props = defineProps({
+        title: {
+        type: String,
+        required: true
+        },
+        message: {
+            type: String,
+            required: true,
+        },
+    });
+</script>
