442bcd38277168d6dd22410035b08657bc7b9287
skhali <sev.jocks@yahoo.fr>
2024-09-09 09:34:32 +0200

    ✨ Etape 04 : Ajout d'un buton au milieu de l'écran et organisation des notifications sous forme de liste en bas à droite

--- homeys-test-front-master/src/App.vue
+++ homeys-test-front-master/src/App.vue
@@ -1,34 +1,49 @@
 <script setup>
 import BaseNotification from './components/BaseNotification.vue';
+import { ref } from 'vue';
+//Liste de Notifications
+const notificationsList = ref([]);
+
+//Types de notifications
+const types = ['danger', 'warning', 'success', 'info'];
+
+/*
+  addNotification(BaseNotification) :
+  Ajoute une notification à la liste des notifications
+*/
+const addNotification = (newNotification) => {
+  notificationsList.value.push(newNotification);
+}
+
+/*
+  randomNotfif() :
+  Crée une notification aléatoire et l'ajoute à la liste ds notifications.
+  Le titre et le message sont génériques.
+*/
+const randomNotif = () => {
+  addNotification({
+    title : 'Modal Window',
+    message : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sodales dictum nibh id commodo',
+    type : types[Math.floor(Math.random() * types.length)]
+  })
+}
 
 </script>
 
 <template>
   <!-- Affichage d'une notification au choix : j'ai choisis la notification d'information -->
-  <!-- Pour cette deuxième étape je n'utilise pas encore de boucle ni de liste -->
-  <main>
-    <BaseNotification
-    :title="'Modal Window'"
-    :message="'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sodales dictum nibh id commodo'"
-    :type="'danger'"
-    ></BaseNotification>
-    <br/>
-    <BaseNotification
-    :title="'Modal Window'"
-    :message="'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sodales dictum nibh id commodo'"
-    :type="'warning'"
-    ></BaseNotification>
-    <br/>
-    <BaseNotification
-    :title="'Modal Window'"
-    :message="'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sodales dictum nibh id commodo'"
-    :type="'success'"
-    ></BaseNotification>
-    <br/>
-    <BaseNotification
-    :title="'Modal Window'"
-    :message="'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sodales dictum nibh id commodo'"
-    :type="'info'"
-    ></BaseNotification>
+  <main >
+    <div class="random-notification-button-div">
+      <button  @click="randomNotif()"> Générer une notification</button>
+    </div>
+      
+    <div class="notifications-list">
+      <BaseNotification v-for="(notif, index) in notificationsList"
+      :key="index"
+      :title="notif.title"
+      :message="notif.message"
+      :type="notif.type"
+      ></BaseNotification>
+    </div>
   </main>
 </template>
